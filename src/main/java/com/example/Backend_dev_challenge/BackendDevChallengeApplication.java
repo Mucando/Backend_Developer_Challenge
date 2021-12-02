55b860e1cda1e65c910097ce0dedb390d1a90d4f
Odnacum <43956724+Odnacum@users.noreply.github.com>
2021-12-02 16:48:07 +0200

    Descricao do projecto

--- src/main/java/com/example/Backend_dev_challenge/BackendDevChallengeApplication.java
+++ src/main/java/com/example/Backend_dev_challenge/BackendDevChallengeApplication.java
@@ -2,12 +2,18 @@ package com.example.Backend_dev_challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BackendDevChallengeApplication {
+public class BackendDevChallengeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendDevChallengeApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BackendDevChallengeApplication.class);
+	}
 }
